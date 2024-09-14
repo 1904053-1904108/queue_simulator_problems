28587d862cccf1ce2b0747b5adaf69608a5d9130
D-R0Y <tensedaf@gmail.com>
2024-09-15 00:27:09 +0600

    main and grocery queue added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int tellers = 3;
-        int maxLength = 5;
-        int simulationMinutes = 200;
-        int simulationSeconds = simulationMinutes / 10;
-        BankQueue bankQueue = new BankQueue(maxLength);
-        QueueSimulator simulator = new QueueSimulator(bankQueue, simulationSeconds, tellers);
+        int bankmaxLength = 5;
+        int grocerymaxLength = 2;
+        int numQueues = 3;
+        int simulationMinutes = 2;
+        int simulationSeconds = simulationMinutes * 60;
+        GroceryQueue groceryQueue = new GroceryQueue(numQueues, grocerymaxLength);
+        BankQueue bankQueue = new BankQueue(tellers, bankmaxLength);
+        QueueSimulator simulator = new QueueSimulator(bankQueue, groceryQueue, simulationSeconds);
         simulator.startSimulation();
     }
 }
